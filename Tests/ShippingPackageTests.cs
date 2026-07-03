ff38bd7e78a0a1726ae1bc2d6167099647b81bfc
nguyenduchuy19012004 <zzcz4991@gmail.com>
2026-07-03 19:26:20 +0700

    feat: derive GHN package size from kilograms

---
+++ Tests/ShippingPackageTests.cs
@@ -0,0 +1,39 @@
+using Fruitables.Models;
+using Xunit;
+
+namespace Fruitables.Tests;
+
+public class ShippingPackageTests
+{
+    [Theory]
+    [InlineData(1, 1000, 20, 15, 10)]
+    [InlineData(2, 2000, 20, 15, 10)]
+    [InlineData(3, 3000, 30, 20, 15)]
+    [InlineData(5, 5000, 30, 20, 15)]
+    [InlineData(6, 6000, 40, 30, 20)]
+    public void FromTotalKg_UsesFruitBoxTiers(
+        int totalKg,
+        int expectedWeight,
+        int expectedLength,
+        int expectedWidth,
+        int expectedHeight)
+    {
+        var package = ShippingPackage.FromTotalKg(totalKg);
+
+        Assert.Equal(expectedWeight, package.Weight);
+        Assert.Equal(expectedLength, package.Length);
+        Assert.Equal(expectedWidth, package.Width);
+        Assert.Equal(expectedHeight, package.Height);
+    }
+
+    [Fact]
+    public void FromTotalKg_ClampsZeroAndNegativeWeightToZero()
+    {
+        var package = ShippingPackage.FromTotalKg(0);
+
+        Assert.Equal(0, package.Weight);
+        Assert.Equal(20, package.Length);
+        Assert.Equal(15, package.Width);
+        Assert.Equal(10, package.Height);
+    }
+}
